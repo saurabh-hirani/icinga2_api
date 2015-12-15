c90661feec0dc7eb51e0f06810d3e7874352276f
Saurabh Hirani <saurabh.hirani@gmail.com>
2015-12-15 18:29:36 +0530

    adding install dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 setup(
   name='icinga2_api',
   license='MIT',
-  version='0.1',
+  version='0.2',
   url='https://github.com/saurabh-hirani/icinga2_api',
   description=('Python library and command-line support for the icinga2 API'),
   author='Saurabh Hirani',
   author_email='saurabh.hirani@gmail.com',
   packages=find_packages(),
+  install_requires=[
+    'click',
+    'requests',
+    'yaml',
+    'json'
+  ],
   entry_points = {
     'console_scripts': [
       'icinga2_api = icinga2_api.cmdline:icinga2_api',
